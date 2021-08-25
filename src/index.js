047c76f1543fe9f9a09f287f563816ef2a9ee8cd
mpreis82 <mpreis82@gmail.com>
2021-08-25 11:30:05 -0300

    Issues solving

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ app.use(express.json());
 
 const repositories = [];
 
+
 app.get("/repositories", (request, response) => {
   return response.json(repositories);
 });
+
 
 app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body
@@ -23,14 +25,19 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
+  repositories.push(repository);
+
   return response.json(repository);
 });
 
+
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, techs, url } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const updatedRepository = { title, techs, url }
+
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
@@ -43,12 +50,13 @@ app.put("/repositories/:id", (request, response) => {
   return response.json(repository);
 });
 
+
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -56,6 +64,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   return response.status(204).send();
 });
+
 
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
@@ -68,7 +77,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json({ likes });
 });
 
 module.exports = app;
